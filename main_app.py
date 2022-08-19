f97719080311ea444a7bdea868bd3142757a9d11
Alby <albychawula@gmail.com>
2022-08-19 12:51:26 +0800

    Moved each frame to its own Class. Prevents having all tkinter widgets between the frames in the same namespace. Makes for better organisation of the code with regards to what methods go where and what class does what.

--- main_app.py
+++ main_app.py
@@ -5,9 +5,82 @@ import json
 from handle_json import JSONHandler
 from handle_json import JSONTopicHandler
 
-class IntroFrame():
+class BasicFrame():
     def __init__(self, parent):
-        self.intro_frame = tk.Frame(master = parent)
+        self.base_frame = tk.Frame(master = parent)
+
+    def show(self):
+        self.base_frame.grid()
+        
+    def remove(self):
+        self.base_frame.grid_remove()
+
+class IntroFrame(BasicFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.intro_label = None
+
+        self.build_intro_screen()
+
+    def build_intro_screen(self):
+        gui_reference_data = JSONHandler.get_js("gui_reference.json")
+
+        self.intro_label = tk.Label(master = self.base_frame, text = gui_reference_data["intro_frame"][0]["main_text"])
+        self.intro_label.grid(column = 0, row = 0)
+
+class TopicSelectFrame(BasicFrame):
+    def __init__(self, parent, main_app):
+        super().__init__(parent)
+
+        self.top_label = None
+        self.topic_buttons = []
+        self.next_screen_button = None
+        self.back_to_start_button = None
+
+        self.build_topic_select_screen(main_app)
+
+    def build_topic_select_screen(self, main_app):
+        topic_file = JSONTopicHandler("topic.json")
+        print("Topics in file: ")
+        topic_file.topic_string()
+
+        self.top_label = tk.Label(master = self.base_frame, text = "Topics from file: ")
+        self.top_label.grid(column = 0, row = 1)
+
+        topic_row = 1
+        for topic in topic_file.topics:
+            self.topic_buttons.append(tk.Button(master = self.base_frame, 
+                                            text = topic, 
+                                            justify = tk.LEFT, 
+                                            wraplength = 145,
+                                            width = 20))
+            self.topic_buttons[topic_row - 1].grid(column = 1, row = topic_row, sticky = tk.W)
+            topic_row += 1
+        
+        self.next_screen_button = tk.Button(master = self.base_frame, text = "Run prompts",
+                                                command = lambda: self.start_prompts(main_app))
+        self.next_screen_button.grid(column = 0, row = topic_row + 1)
+
+        self.back_to_start_button = tk.Button(master = self.base_frame, 
+                                                text = "Back to intro!", 
+                                                command = lambda: main_app.update_current_screen(0, main_app.current_screen))
+        self.back_to_start_button.grid(column = 0, row = 0)
+
+    def start_prompts(self, main_app):
+        main_app.update_current_screen(2, main_app.current_screen)
+
+class DisplayPrompts(BasicFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.top_label = None
+
+        self.build_run_prompts_screen()
+
+    def build_run_prompts_screen(self):
+        self.top_label = tk.Label(master = self.base_frame, text = "Running through prompts!")
+        self.top_label.grid(row = 0, column = 0)
+
 
 class MainApp:
     def __init__(self, parent, name = None):
@@ -15,12 +88,12 @@ class MainApp:
         self.current_screen = 0
 
         self.intro_frame = IntroFrame(self.main_window)
+        self.topic_select_frame = TopicSelectFrame(self.main_window, self)
+        self.display_prompts_frame = DisplayPrompts(self.main_window)        
 
-        self.topic_select_frame = tk.Frame(master = self.main_window)
-        self.run_prompts_frame = tk.Frame(master = self.main_window)
-
-        self.initalise_screens()
         self.update_current_screen(0)
+
+        self.set_callbacks()
 
     # Will change the current screen that is display.
     # WARNING - If no current_screen is entered (no argument passed) no screen will be unpacked
@@ -28,27 +101,25 @@ class MainApp:
         # Unpack/destroy current frame in prep for building next frame
 
         if current_screen == 0:
-            self.intro_frame.grid_remove()
+            self.intro_frame.remove()
         elif current_screen == 1:
-            self.topic_select_frame.grid_remove()
+            self.topic_select_frame.remove()
         elif current_screen == 2:
-            self.run_prompts_frame.grid_remove()
+            self.display_prompts_frame.remove()
 
         # Builds the next frame
         if new_screen == 0:
-            self.intro_frame.grid()
+            self.intro_frame.show()
             self.current_screen = new_screen
         elif new_screen == 1:
-            self.topic_select_frame.grid()
+            self.topic_select_frame.show()
             self.current_screen = new_screen
         elif new_screen == 2:
-            self.run_prompts_frame.grid()
+            self.display_prompts_frame.show()
             self.current_screen = new_screen
 
         if current_screen == -1:
             print("No previous screen, no screen unpacked.")
-        
-        self.set_callbacks()
     
     def set_callbacks(self):
         def handle_intro_callbacks(event, self = self):
@@ -58,55 +129,7 @@ class MainApp:
 
         self.main_window.bind("<Any-Key>", handle_intro_callbacks) # Will trigger while any frame is up - perform check in callback method
 
-    # Builds all the screens/widgets but does not place them onto the main_window
-    def initalise_screens(self):
-        self.build_intro_screen()
-        self.build_topic_select_screen()
-        self.build_run_prompts_screen()
-
-    def build_intro_screen(self):
-        gui_reference_data = JSONHandler.get_js("gui_reference.json")
-
-        self.intro_label = tk.Label(master = self.intro_frame, text = gui_reference_data["intro_frame"][0]["main_text"])
-        self.intro_label.grid(column = 0, row = 0)
-
-    def build_topic_select_screen(self):
-        topic_file = JSONTopicHandler("topic.json")
-        print("Topics in file: ")
-        topic_file.topic_string()
-
-        self.top_label = tk.Label(master = self.topic_select_frame, text = "Topics from file: ")
-        self.top_label.grid(column = 0, row = 1)
-
-        topic_row = 1
-        topic_label = []
-        for topic in topic_file.topics:
-            topic_label.append(tk.Button(master = self.topic_select_frame, 
-                                            text = topic, 
-                                            justify = tk.LEFT, 
-                                            wraplength = 145,
-                                            width = 20,
-                                            command = ))
-            topic_label[topic_row - 1].grid(column = 1, row = topic_row, sticky = tk.W)
-            topic_row += 1
-        
-        next_screen_button = tk.Button(master = self.topic_select_frame, text = "Run prompts",
-                                            command = lambda: self.start_prompts())
-        next_screen_button.grid(column = 0, row = topic_row + 1)
-
-        self.back_to_start_button = tk.Button(master = self.topic_select_frame, 
-                                                text = "Back to intro!", 
-                                                command = lambda: self.update_current_screen(0, self.current_screen))
-        self.back_to_start_button.grid(column = 0, row = 0)
     
-    def build_run_prompts_screen(self):
-        run_prompts = tk.Label(master = self.run_prompts_frame, text = "Running through prompts!")
-        run_prompts.grid(row = 0, column = 0)
-
-    def start_prompts(self, topic):
-
-
-        self.update_current_screen(2, self.current_screen)
 
 
 if __name__ == "__main__":
